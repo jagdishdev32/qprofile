7e40180767cc0dd7da41efed515b7a463778041a
Jatin Besoya <jatinbesoya32@gmail.com>
2021-01-18 14:28:15 +0530

    first commit

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Jatin Besoya",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Jatin Besoya and I'm a Software Engineer!",
+  githubUrl: "https://github.com/jagdishdev32/",
+  linkedinUrl: "https://linkedin.com/in/jatin-besoya-0523171b9",
+//   cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  cvLink: "https://linkedin.com/in/jatin-besoya-0523171b9",
+};
